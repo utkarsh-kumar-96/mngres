f79c619d8de83f7f927c4fb9a0b2b8e375766cb7
Utkarsh Kumar <utkarshkumar199626@gmail.com>
2022-02-21 13:19:16 +0000

    intial commit

---
+++ src/main/java/com/utkarsh/mngres/MngresApplication.java
@@ -0,0 +1,13 @@
+package com.utkarsh.mngres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/utkarsh/mngres/MngresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utkarsh.mngres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MngresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
